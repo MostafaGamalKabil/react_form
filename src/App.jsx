36169ce25bf2d34ed5d6979fdeb112a5aeb98b36
Mostafa Kabil <kabilmostafa060@gmail.com>
2025-03-29 01:57:12 +0200

    using useState To Change Name And Age And Counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,35 @@
-
 import './App.css'
-
+import { useState } from 'react';
 function App() {
-let person = "mostafa";
 
-const ChangeName = () => {
- person = "ElRAYK";
- console.log("ElRAYK");
-}
+  const [person , ChangeName] = useState("Mostafa gamal")
+  const [age , ChangeAge] = useState(28)
+  const [increment , setIncrement] = useState(0)
+
+
   return (
     <>
       
     <h1>My name is : {person} </h1>
-        <button onClick={ChangeName} >
+        <button onClick={() => {ChangeName("ELRAYEK")}} >
           Change name
         </button>
-      
+      <br />
+      <br />
+      <br />
+          
+    <h1>My name is : {age} </h1>
+        <button onClick={() => {ChangeAge(20)}} >
+          Change name
+        </button>
+      <br />
+      <br />
+      <br />
+          
+
+        <button onClick={() => {setIncrement(increment+1)}} >
+          count {increment}
+        </button>
 
     
     </>
