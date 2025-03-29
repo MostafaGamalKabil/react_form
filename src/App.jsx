15bd1674ddd3536165bdd751481628829ebbcee6
Mostafa Kabil <kabilmostafa060@gmail.com>
2025-03-29 04:01:01 +0200

    Create 4 btn to change do dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
 import './App.css'
+import './theme.css'
 import { useState } from 'react';
 function App() {
 
   const [person , ChangeName] = useState("Mostafa gamal")
   const [age , ChangeAge] = useState(28)
   const [increment , setIncrement] = useState(0)
-
+  const [theme , settheme] = useState("")
 
   return (
     <>
+
+<div className={`App ${theme}`}>
+<div>
+        <button onClick={() => {settheme("light")}} style={{marginRight: "26px"}}>Light</button>
+        <button onClick={() => {settheme("dark")}} style={{marginRight: "26px"}}>Dark</button>
+        <button onClick={() => {settheme("grey")}} style={{marginRight: "26px"}}>Grey</button>
+        <button onClick={() => {settheme("pink")}} style={{marginRight: "26px"}}>Pink</button>
+</div>
+
+
+
       
-    <h1>My name is : {person} </h1>
+    <h1  style={{marginTop: "66px"}}>My name is : {person} </h1>
         <button onClick={() => {ChangeName("ELRAYEK")}} >
           Change name
         </button>
@@ -31,7 +43,7 @@ function App() {
           count {increment}
         </button>
 
-    
+        </div>
     </>
   )
 }
