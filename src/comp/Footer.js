ad2dacae761ff6a737abd8fc9a6bb4525f375178
Mostafa Kabil <kabilmostafa060@gmail.com>
2025-04-13 04:13:21 +0200

    Toggle datk mode in all Pages using Context

---
+++ src/comp/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import    './Footer.css';
+
+const Footer = () => {
+  return (
+<div className="myfooter">
+      <footer className="ali   ">
+        Designed and developed by Web Devs
+        <span>🧡</span>
+      </footer>
+</div>
+  );
+};
+
+export default Footer;
---
+++ src/context/ThemeContext.js
@@ -0,0 +1,30 @@
+import { type } from "@testing-library/user-event/dist/type";
+import { createContext, useReducer } from "react";
+const ThemeContexttt = createContext();
+
+const initialData = { theme: "Light" };
+const reducer = (firstState , action) => {
+  switch (action.type) {
+    case "ToggleTheme":
+      return{...firstState , theme : action.newValue};
+  
+    default:
+      return firstState;
+  }
+}
+
+export function ThemeProvider({ children }) {
+  const [firstState, dispatch] = useReducer(reducer, initialData);
+
+  const ToggleTheme = (newTheme) => {
+    dispatch({type : "ToggleTheme" , newValue : newTheme })
+  }
+
+  return (
+     <ThemeContexttt.Provider value={{ ...firstState , ToggleTheme}}>
+      {children}
+     </ThemeContexttt.Provider>
+  );
+}
+
+export default ThemeContexttt;
