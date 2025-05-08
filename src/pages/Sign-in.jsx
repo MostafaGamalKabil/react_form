951123413510b02aa807b24512ab69de08a3e5f4
Mostafa Kabil <kabilmostafa060@gmail.com>
2025-05-09 02:33:24 +0300

    forgot password

--- src/pages/Sign-in.jsx
+++ src/pages/Sign-in.jsx
@@ -3,16 +3,21 @@ import Footer from "../comp/Footer";
 import { Helmet } from "react-helmet-async";
 import { Link } from "react-router-dom";
 // sign-in importing
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from '../firebase/config';
+import { signInWithEmailAndPassword , sendPasswordResetEmail} from "firebase/auth";
+import { auth } from "../firebase/config";
 import { useState } from "react";
 import { useNavigate } from "react-router";
+import "./signin.css";
+
 const Signin = () => {
   let navigate = useNavigate();
   const [email, setemail] = useState("");
   const [password, setpassword] = useState("");
+  const [showForm, setshowForm] = useState("");
   const [hasError, sethasError] = useState(false);
+  const [resetPass, setresetPass] = useState(false);
   const [firebaseError, setfirebaseError] = useState(false);
+  const [showSendEmail, setshowSendEmail] = useState(false);
 
   return (
     <>
@@ -21,69 +26,126 @@ const Signin = () => {
         <meta name="description" content="Sign Up" />
       </Helmet>
       <Header />
-     <main>
+      <main>
+        <form className={`forgot-password ${showForm}`}>
+          <div
+            onClick={() => {
+              setshowForm("");
+            }}
+            className="close"
+          >
+            <i className="fa-solid fa-xmark"></i>
+          </div>
+          <input onChange={(eo) => {
+            setresetPass(eo.target.value)
+          }} type="email" placeholder="E-mail" />
+          <button
+            onClick={(eo) => {
+              eo.preventDefault();
 
-      <form>
+              sendPasswordResetEmail(auth, resetPass)
+                .then(() => {
+                  setshowSendEmail(true); // لما التغير يبقى ترو يظهر
+                })
+                .catch((error) => {
+                  const errorCode = error.code;
+                  const errorMessage = error.message;
+                  // ..
+                });
+            }}
+          >
+            Reset Password
+          </button>
 
-<input onChange={(eo) => {
-  setemail(eo.target.value)
-}} type="email" placeholder="Email: "  required/>
+          {showSendEmail && (
+            <p className="check-email">
+              Please check your email to reset your password
+            </p>
+            // هنا انا بقوله المتغير دا يا اما هو ترو يا اما هو فولس
+          )}
+        </form>
 
-<input onChange={(eo) => {
-  setpassword(eo.target.value)
-}}  type="password" placeholder="Password: "  required/>
-<button onClick={(eo) => {
-eo.preventDefault()
-  signInWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    navigate("/");
-    // ...
-  })
+        <form>
+          <input
+            onChange={(eo) => {
+              setemail(eo.target.value);
+            }}
+            type="email"
+            placeholder="Email: "
+            required
+          />
 
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
+          <input
+            onChange={(eo) => {
+              setpassword(eo.target.value);
+            }}
+            type="password"
+            placeholder="Password: "
+            required
+          />
+          <button
+            onClick={(eo) => {
+              eo.preventDefault();
+              signInWithEmailAndPassword(auth, email, password)
+                .then((userCredential) => {
+                  // Signed in
+                  const user = userCredential.user;
+                  navigate("/");
+                  // ...
+                })
 
-    switch (errorCode) {
-      case "auth/invalid-email":
-        setfirebaseError("Invalid Email")
-        
-        break;
-      case "auth/invalid-credential":
-        setfirebaseError("User Not Found")
-        
-        break;
-      case "auth/invalid-password-hash":
-        setfirebaseError("Invalid Password")
-        
-        break;
-      case "auth/too-many-requests":
-        setfirebaseError("Too many requstes , please try again later")
-        
-        break;
-    
-      default:
-        setfirebaseError("Please Check your email & password")
-        break;
-    }
+                .catch((error) => {
+                  const errorCode = error.code;
+                  const errorMessage = error.message;
 
+                  switch (errorCode) {
+                    case "auth/invalid-email":
+                      setfirebaseError("Invalid Email");
 
-    
-    sethasError(errorMessage)
-  });
-}}>Sign in
-</button>
-<p className="account">
-  Dont't have an account <Link to="/signup">Sign up</Link>
-</p>
+                      break;
+                    case "auth/invalid-credential":
+                      setfirebaseError("User Not Found");
 
-{hasError && <p className="ErrorMessage">{firebaseError}</p>}
-      </form>
+                      break;
+                    case "auth/invalid-password-hash":
+                      setfirebaseError("Invalid Password");
 
-     </main>
-      
+                      break;
+                    case "auth/too-many-requests":
+                      setfirebaseError(
+                        "Too many requstes , please try again later"
+                      );
+
+                      break;
+
+                    default:
+                      setfirebaseError("Please Check your email & password");
+                      break;
+                  }
+
+                  sethasError(errorMessage);
+                });
+            }}
+          >
+            Sign in
+          </button>
+          <p className="account">
+            Dont't have an account <Link to="/signup">Sign up</Link>
+          </p>
+
+          <p
+            onClick={() => {
+              setshowForm("show-forgot-password");
+            }}
+            className="forgot-pass"
+          >
+            forgot password ?
+          </p>
+
+          {hasError && <p className="ErrorMessage">{firebaseError}</p>}
+        </form>
+      </main>
+
       <Footer />
     </>
   );
